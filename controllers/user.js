4348d9618a4e7d00af8853487a81f757c8b62e84
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-05-29 13:10:04 -0400

    adds create-save, getOne-getById, deleteOne-delete to user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,32 @@
 const User = require('../models/user');
 
-module.exports.add = async (req, res) => {
+module.exports.create = async (req, res) => {
     const user = User.from(req.body);
     const id = await user.save();
     res.json({
         message: 'Create Account Succesful',
+        payload: {
+            id
+        }
+    });
+};
+
+module.exports.getOne = async (req, res) => {
+    const { id } = req.params;
+    const user = await User.getById(id);
+    res.json({
+        message: 'Success',
+        payload: {
+            user
+        }
+    })
+};
+
+module.exports.deleteOne = async (req, res) => {
+    const { id } = req.params;
+    const result = await User.delete(id);
+    res.json({
+        message: 'Success',
         payload: {
             id
         }
