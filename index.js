342bdd135771f58e82793602c5613479b90c9df1
Ubuntu <ubuntu@ip-172-31-83-147.ec2.internal>
2019-05-31 20:19:22 +0000

    adds path and static for react front-end

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // const es6Renderer = require('express-es6-template-engine');
 // const http = require('http');
