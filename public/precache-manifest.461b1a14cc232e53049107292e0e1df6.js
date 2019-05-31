f3a0230160a44ec251bd202b1ef881c71a615e74
Anthony Moss <asmoss93@gmail.com>
2019-05-31 15:09:57 -0400

    connect backend to frontend

---
+++ public/precache-manifest.461b1a14cc232e53049107292e0e1df6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692b72410e4d0942f1bad0940b8ee7cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2e0d8f041177563732f",
+    "url": "/static/css/main.538c2e91.chunk.css"
+  },
+  {
+    "revision": "5f2436d5a710f0ef6a50",
+    "url": "/static/js/2.71cff70a.chunk.js"
+  },
+  {
+    "revision": "a2e0d8f041177563732f",
+    "url": "/static/js/main.f89b43aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23b8c70fd79bcc5d187b1ed3ec6f2f8e",
+    "url": "/static/media/$AH-Dragoon.23b8c70f.png"
+  },
+  {
+    "revision": "2710b029c52aff1c2d70132a6f081e39",
+    "url": "/static/media/cave-map-true.2710b029.png"
+  },
+  {
+    "revision": "8f5c1a86c7cfaee5c548375ae63ab732",
+    "url": "/static/media/plains.8f5c1a86.png"
+  },
+  {
+    "revision": "050429b9517719320412135c139a8c54",
+    "url": "/static/media/sakura_dungeon_05.050429b9.jpg"
+  }
+]);
